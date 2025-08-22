8379f287c1c5fb444b0a470bc3f384bab971df6f
Pavan-Srivathsa <62050941+Pavan-Srivathsa@users.noreply.github.com>
2025-08-22 09:14:43 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@
 
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // uncomment if you use React
+// import react from '@vitejs/plugin-react' // uncomment if you use React
 
 export default defineConfig({
-  plugins: [react()],                     // uncomment if you use React
+  // plugins: [react()],                     // uncomment if you use React
   base: '/',                                 // user site (username.github.io) lives at the root
   build: { outDir: 'docs', emptyOutDir: true }
 })
